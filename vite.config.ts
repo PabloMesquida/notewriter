241c167e46ac696604e624e0a128c3a92f0a165b
Pablo Mesquida <pablomesquida67@gmail.com>
2023-05-24 21:36:28 +0200

    vercel.json-8

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://notewriter-backend.vercel.app",
+      "/api": "https://notewriter-backend.vercel.app/",
     },
   },
   plugins: [react()],
