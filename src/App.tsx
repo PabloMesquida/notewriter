377cbf5eefcae28cd0e2ef1ca26edf1deb7acf9c
Pablo Mesquida <pablomesquida67@gmail.com>
2023-05-29 18:40:08 +0200

    delete-note

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as NotesApi from "./network/notes_api";
 import { useEffect, useState } from "react";
 import { Note as NoteModel } from "./models/note";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa";
 import Note from "./components/Note";
 import styles from "./styles/NotePage.module.css";
 import styleUtils from "./styles/utils.module.css";
@@ -24,18 +25,33 @@ function App() {
     loadNotes();
   }, []);
 
+  async function deleteNote(note: NoteModel) {
+    try {
+      await NotesApi.deleteNote(note._id);
+      setNotes(notes.filter((existingNote) => existingNote._id !== note._id));
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+
   return (
     <Container>
       <Button
-        className={`${styleUtils.blockCenter} mb-4`}
+        className={`${styleUtils.blockCenter} ${styleUtils.flexCenter} mb-4`}
         onClick={() => setShowNoteDialog(true)}
       >
+        <FaPlus />
         Add new note
       </Button>
       <Row xs={1} md={2} xl={3} className="g-4">
         {notes.map((note) => (
           <Col key={note._id}>
-            <Note note={note} className={styles.note} />
+            <Note
+              note={note}
+              className={styles.note}
+              onDeleteNoteClicked={deleteNote}
+            />
           </Col>
         ))}
       </Row>
